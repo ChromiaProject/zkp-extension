a18cc299d41e7122b423b6a248df8fa4dd7f76f7
Mikael Ståldal <mikael.staldal@chromaway.com>
2026-01-27 13:23:22 +0100

    Kotlin 2.3

--- src/main/kotlin/net/postchain/zkp/plonk/PlonkVerificationKey.kt
+++ src/main/kotlin/net/postchain/zkp/plonk/PlonkVerificationKey.kt
@@ -1,40 +1,40 @@
 package net.postchain.zkp.plonk
 
+import net.postchain.gtv.mapper.Name
 import net.postchain.zkp.curve.BLS12381
 import net.postchain.zkp.curve.BN128
 import net.postchain.zkp.curve.G1Point
 import net.postchain.zkp.curve.G2Point
-import net.postchain.gtv.mapper.Name
 import java.math.BigInteger
 
 data class PlonkVerificationKey(
-        @Name("curve")
+        @param:Name("curve")
         val curveName: CurveName,
-        @Name("nPublic")
+        @param:Name("nPublic")
         val nPublicLong: Long,
-        @Name("power")
+        @param:Name("power")
         val powerLong: Long,
-        @Name("Qm")
+        @param:Name("Qm")
         val Qm: G1Point,
-        @Name("Ql")
+        @param:Name("Ql")
         val Ql: G1Point,
-        @Name("Qr")
+        @param:Name("Qr")
         val Qr: G1Point,
-        @Name("Qo")
+        @param:Name("Qo")
         val Qo: G1Point,
-        @Name("Qc")
+        @param:Name("Qc")
         val Qc: G1Point,
-        @Name("S1")
+        @param:Name("S1")
         val S1: G1Point,
-        @Name("S2")
+        @param:Name("S2")
         val S2: G1Point,
-        @Name("S3")
+        @param:Name("S3")
         val S3: G1Point,
-        @Name("k1")
+        @param:Name("k1")
         val k1: BigInteger,
-        @Name("k2")
+        @param:Name("k2")
         val k2: BigInteger,
-        @Name("X_2")
+        @param:Name("X_2")
         val X2: G2Point
 ) {
     enum class CurveName {
